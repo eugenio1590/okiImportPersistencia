a161d686694d53ea2623cb04d593896c61513dfa
Usuario <eugeniohernandez17@gmail.com>
2015-10-18 23:11:12 -0430

    Cambios en los parametros del servicio para enviar el requerimiento al proveedor.

--- src/main/java/com/okiimport/app/service/mail/MailProveedor.java
+++ src/main/java/com/okiimport/app/service/mail/MailProveedor.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import com.okiimport.app.model.DetalleCotizacion;
 import com.okiimport.app.model.Proveedor;
+import com.okiimport.app.model.Requerimiento;
 
 public interface MailProveedor {
 	void registrarSolicitudProveedor(final Proveedor proveedor, final MailService mailService);
-	void enviarRequerimientoProveedor(final Proveedor proveedor, final List<DetalleCotizacion> detallesCotizacion, 
-			final MailService mailService);
+	void enviarRequerimientoProveedor(final Proveedor proveedor, final Requerimiento requerimiento, 
+			final List<DetalleCotizacion> detallesCotizacion, final MailService mailService);
 }
